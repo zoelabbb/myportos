a06660da5f16f032fbbee3c55227060b6bd5f0a4
Ryu <zoelabbb@gmail.com>
2024-02-05 22:05:29 +0700

    Update Logo

--- src/partials/Navbar.tsx
+++ src/partials/Navbar.tsx
@@ -12,22 +12,7 @@ const Navbar = () => (
       <a href="/">
         <Logo
           icon={
-            <svg
-              className="mr-1 h-10 w-10 stroke-cyan-600"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              strokeWidth="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M0 0h24v24H0z" stroke="none"></path>
-              <rect x="3" y="12" width="6" height="8" rx="1"></rect>
-              <rect x="9" y="8" width="6" height="12" rx="1"></rect>
-              <rect x="15" y="4" width="6" height="16" rx="1"></rect>
-              <path d="M4 20h14"></path>
-            </svg>
-          }
+            <svg className="h-8 w-8 text-rose-500" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <path d="M5 7l5 5l-5 5" />  <line x1="12" y1="19" x2="19" y2="19" /></svg>}
           name="Alif Ryuu Portofolio"
         />
       </a>
